cfcf7a9032821d3e32a66d0f3167542e3c539001
czihao <czihao@wustl.edu>
2023-07-18 18:38:10 -0500

    updated mnist not working

--- test_hdf.py
+++ test_hdf.py
@@ -3,18 +3,36 @@ import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
+from keras.datasets import mnist
+
 torch.manual_seed(0)
 torch.use_deterministic_algorithms(True)
 
-num_neurons = 100
-num_pattern = 10
-patterns = 2*torch.rand((num_pattern, num_neurons))-1
+(train_X, train_y), (test_X, test_y) = mnist.load_data()
+
+num_neurons = train_X.shape[1]**2
+num_pattern = 3
+patterns = np.zeros((num_pattern, num_neurons))
+idx = np.random.permutation(np.arange(train_X.shape[0]))
+patterns_m = []
+for i in range(num_pattern):
+    patterns[i,:] = train_X[idx[i]].flatten()
+    mask = np.random.rand(train_X.shape[2], train_X.shape[2])
+    mask[mask>0.3] = 0
+    pattern_m = (train_X[idx[i]] * mask).flatten()
+    patterns_m.append(pattern_m)
+
+patterns = torch.tensor(patterns, dtype=torch.float)
+# patterns = 2*torch.rand((num_pattern, num_neurons))-1
 patterns = torch.where(patterns > 0, float(1),float(-1))
+
+
 
 class hpf(nn.Module):
     def __init__(self):
         super().__init__()
-        self.x = nn.Parameter(2*torch.rand((num_neurons, 1), requires_grad=True)-1)
+        self.x = nn.Parameter(torch.tensor(patterns_m[0], dtype=torch.float), requires_grad=True)
+        # self.x = nn.Parameter(2*torch.rand((num_neurons, 1), requires_grad=True)-1)
         ##### zero activation #####
         # self.x.data = torch.where(self.x.data > 0, float(1), float(-1))
         ##### tanh #####
@@ -61,7 +79,7 @@ quad = hpf()
 opt = torch.optim.Adam(quad.parameters(), lr=1e-3)
 
 loss = []
-for i in range(2000):
+for i in range(1000):
     y = quad()
     loss.append(y.item())
     if i % 100 == 0:
@@ -76,13 +94,23 @@ for i in range(2000):
     ##### tanh #####
     quad.x.data[idx] = F.tanh(quad.x.data[idx])
     
-    if i % 100 == 0:
-        pattern = 2*torch.rand((num_neurons))-1
-        pattern = torch.where(pattern > 0, float(1),float(-1))
-        # num_pattern += 1
-        quad.Q = (num_pattern-1)/num_pattern * quad.Q + 1/num_pattern * torch.outer(pattern, pattern)
+    # if i % 100 == 0:
+    #     pattern = 2*torch.rand((num_neurons))-1
+    #     pattern = torch.where(pattern > 0, float(1),float(-1))
+    #     # num_pattern += 1
+    #     quad.Q = (num_pattern-1)/num_pattern * quad.Q + 1/num_pattern * torch.outer(pattern, pattern)
     
+fig = plt.figure()
+ax1 = fig.add_subplot(131)
+ax2 = fig.add_subplot(132)
+ax3 = fig.add_subplot(133)
 
+ax1.imshow(train_X[idx[0]], cmap=plt.get_cmap('gray'))
+# plt.show()
+ax2.imshow(patterns_m[0].reshape(train_X.shape[2], train_X.shape[2]) , cmap=plt.get_cmap('gray'))
+# plt.show()
+ax3.imshow(quad.x.data.reshape(train_X.shape[2], train_X.shape[2]), cmap=plt.get_cmap('gray'))
+plt.show()
 
 fig = plt.figure()
 loss_ax = fig.add_subplot(111)
